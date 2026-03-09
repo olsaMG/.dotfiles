46ff431c4bb371f18f190c1b1e7adea92f20c7ca
Oliver Säfström <oliver.safstrom@mobilityguard.com>
2026-03-09 10:02:10 +0100

    filter out .preconfig

--- nvim/.config/nvim/lua/plugins/nvim-tree.lua
+++ nvim/.config/nvim/lua/plugins/nvim-tree.lua
@@ -1,85 +1,88 @@
 return {
-	"nvim-tree/nvim-tree.lua",
-	version = "*",
-	lazy = false,
-	dependencies = {
-		"nvim-tree/nvim-web-devicons",
-	},
-	config = function()
-		require("nvim-tree").setup({
-			filters = {
-				dotfiles = false,
-			},
-			disable_netrw = true,
-			hijack_netrw = true,
-			hijack_cursor = true,
-			hijack_unnamed_buffer_when_opening = false,
-			sync_root_with_cwd = true,
-			update_focused_file = {
-				enable = true,
-				update_root = false,
-			},
-			view = {
-				adaptive_size = true,
-				side = "left",
-				width = 30,
-				preserve_window_proportions = true,
-			},
-			git = {
-				enable = false,
-				ignore = true,
-			},
-			filesystem_watchers = {
-				enable = true,
-			},
-			actions = {
-				open_file = {
-					resize_window = true,
-				},
-			},
-			renderer = {
-				root_folder_label = false,
-				highlight_git = false,
-				highlight_opened_files = "none",
+  "nvim-tree/nvim-tree.lua",
+  version = "*",
+  lazy = false,
+  dependencies = {
+    "nvim-tree/nvim-web-devicons",
+  },
+  config = function()
+    require("nvim-tree").setup({
+      filters = {
+        custom = {
+          "^\\.pre-commit-config\\.yaml$"
+        },
+        dotfiles = false,
+      },
+      disable_netrw = true,
+      hijack_netrw = true,
+      hijack_cursor = true,
+      hijack_unnamed_buffer_when_opening = false,
+      sync_root_with_cwd = true,
+      update_focused_file = {
+        enable = true,
+        update_root = false,
+      },
+      view = {
+        adaptive_size = true,
+        side = "left",
+        width = 30,
+        preserve_window_proportions = true,
+      },
+      git = {
+        enable = false,
+        ignore = true,
+      },
+      filesystem_watchers = {
+        enable = true,
+      },
+      actions = {
+        open_file = {
+          resize_window = true,
+        },
+      },
+      renderer = {
+        root_folder_label = false,
+        highlight_git = false,
+        highlight_opened_files = "none",
 
-				indent_markers = {
-					enable = false,
-				},
+        indent_markers = {
+          enable = false,
+        },
 
-				icons = {
-					show = {
-						file = true,
-						folder = true,
-						folder_arrow = true,
-						git = false,
-					},
+        icons = {
+          show = {
+            file = true,
+            folder = true,
+            folder_arrow = true,
+            git = false,
+          },
 
-					glyphs = {
-						default = "󰈚",
-						symlink = "",
-						folder = {
-							default = "",
-							empty = "",
-							empty_open = "",
-							open = "",
-							symlink = "",
-							symlink_open = "",
-							arrow_open = "",
-							arrow_closed = "",
-						},
-						git = {
-							unstaged = "✗",
-							staged = "✓",
-							unmerged = "",
-							renamed = "➜",
-							untracked = "★",
-							deleted = "",
-							ignored = "◌",
-						},
-					},
-				},
-			},
-		})
-		vim.keymap.set("n", "<leader>e", ":NvimTreeToggle<CR>", { desc = "Toggle Nvim-Tree" })
-	end,
+          glyphs = {
+            default = "󰈚",
+            symlink = "",
+            folder = {
+              default = "",
+              empty = "",
+              empty_open = "",
+              open = "",
+              symlink = "",
+              symlink_open = "",
+              arrow_open = "",
+              arrow_closed = "",
+            },
+            git = {
+              unstaged = "✗",
+              staged = "✓",
+              unmerged = "",
+              renamed = "➜",
+              untracked = "★",
+              deleted = "",
+              ignored = "◌",
+            },
+          },
+        },
+      },
+    })
+    vim.keymap.set("n", "<leader>e", ":NvimTreeToggle<CR>", { desc = "Toggle Nvim-Tree" })
+  end,
 }
